0dde2717a11e3d00d2c91b2726a292ef189cb001
Michael Olatunji <tundexmike@gmail.com>
2020-03-23 15:49:10 +0100

    angular front end working fine in dist except on refresh

--- app.js
+++ app.js
@@ -13,14 +13,13 @@ const invitationReminder = require('./routes/invitationRemainder');
 const papersReminder = require('./routes/papersRemainder');
 // let mailRouter = require('./routes/invitationMail');
 const app = express();
-let port = process.env.PORT || 3000
+let port = process.env.PORT || 8080;
 const passport = require('passport');
 const authenticate = require('./authenticate')
 const config = require('./config');
 
 
-
-
+// 'mongodb+srv://mikeking:mikeking@cluster0-u8mop.mongodb.net/test?retryWrites=true&w=majority'
 const url = config.mongoUrl;
 mongoose.connect(url, {useNewUrlParser: true})
 .then(() => {
@@ -30,28 +29,32 @@ error => {
   console.log('error while connecting to server')
 })
 
-let corsOptions = {
-  origin: 'http://localhost:4200',
-  optionsSuccessStatus: 200
-}
+// let corsOptions = {
+//   origin: 'http://localhost:4200',
+//   optionsSuccessStatus: 200
+// }
 
 // app.use((req, res, next) => {
 //   res.header("Access-Control-Allow-Origin", "*")
 //   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
 //   next();
 // });
+app.use(cors());
 app.use(express.static(path.join(__dirname, '/public')));
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'ejs');
 
-app.use(cors(corsOptions));
 // app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.raw({ limit: '50mb' }));
+app.use(bodyParser.text({limit: '50mb'}));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser());
-
 app.use(passport.initialize());
 
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '/public/index.html'));
+// })
 
 app.use('/users', usersRouter);
 app.use('/api', candidateRouter);
